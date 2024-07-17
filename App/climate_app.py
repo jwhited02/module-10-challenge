638d0c4e6b666402cbaf65bd27be367f388b6523
jwhited02 <166862877+jwhited02@users.noreply.github.com>
2024-07-17 14:04:57 -0500

    Final edits to climate app

--- App/climate_app.py
+++ App/climate_app.py
@@ -1,30 +1,30 @@
 # Import the dependencies.
-from flask import Flask, jsonify
-from sqlalchemy import create_engine, func
+import numpy as np
+import datetime as dt
+
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-import datetime as dt
-import numpy as np
-import os
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-database_path = os.path.abspath("C:/Users/jwhit/OneDrive/excel challenge/module-10-challenge/Resources/hawaii.sqlite")
-engine = create_engine(f"sqlite:///{database_path}")
+engine = create_engine(f"sqlite:///../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 
 # reflect the tables
-Base.prepare(engine, reflect=True)
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
+
 
 #################################################
 # Flask Setup
@@ -35,17 +35,33 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br>"
+        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tobs<br>"
+        f"/api/v1.0/<start><br>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     """Return a list of precipitation data including the date and precipitation value."""
-   
     # Calculate the date one year from the last date in the dataset
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
     one_year_ago = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=365)
 
     # Query for the previous 12 months of precipitation data
-    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
-    one_year_ago = dt.datetime.strptime(last_date, "%Y-%m-%d") - dt.timedelta(days=365)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= one_year_ago).all()
+
+    session.close()
 
     # Convert the query results to a dictionary
     precipitation_data = {date: prcp for date, prcp in results}
@@ -55,9 +71,14 @@ def precipitation():
     # Query all stations
 @app.route("/api/v1.0/stations")
 def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     """Return a list of stations."""
 
     results = session.query(Station.station).all()
+
+    session.close()
 
     # Convert the query results to a list of dictionaries
     stations = list(np.ravel(results))
@@ -66,8 +87,10 @@ def stations():
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    """Return a list of temperature observations (TOBS)
-     for the previous year."""
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of temperature observations (TOBS) for the previous year."""
     
     # Calculate the date one year from the last date in the dataset
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
@@ -75,6 +98,8 @@ def tobs():
 
     # Query the last 12 months of temperature observation data for the most active station
     results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= one_year_ago).all()
+
+    session.close()
  
     # Convert the query results to a list
     tobs_data = list(np.ravel(results))
@@ -83,10 +108,17 @@ def tobs():
 
 @app.route("/api/v1.0/<start>")
 def start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start range."""
+
+    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
 
     # Query the minimum, average, and maximum temperatures for dates greater than or equal to the start date
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).all()
+
+    session.close()
 
     # Convert the query results to a list of dictionaries
     temps = list(np.ravel(results))
@@ -95,10 +127,18 @@ def start(start):
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_end(start, end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
     """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start-end range."""
+
+    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
+    end_date = dt.datetime.strptime(end, "%Y-%m-%d")
 
     # Query the minimum, average, and maximum temperatures for dates between the start and end date inclusive
     results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    session.close()
 
     # Convert the query results to a list of dictionaries
     temps = list(np.ravel(results))
